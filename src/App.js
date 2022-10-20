8283ab41748356402d490e58bed26c3f5b7f8ae1
Lucas <lucasreis64@gmail.com>
2022-10-19 21:30:01 -0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "../Auxiliares/GlobalStyles";
+
 function App() {
-  return 
+  return (
+    <>
+      <GlobalStyle/>  
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Login/>} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/hoje" element={<Hoje />} />
+          <Route path="/habitos" element={<Habitos />} />
+          <Route path="/historico" element={<Historico />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
