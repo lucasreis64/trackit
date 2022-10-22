bd058d6460a76fc9312e2e8c0628f0ccfa0dbaa6
Lucas <lucasreis64@gmail.com>
2022-10-22 08:59:18 -0300

    Começando página "hoje"

--- src/Components/Hoje/Hoje.js
+++ src/Components/Hoje/Hoje.js
@@ -1,26 +1,51 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import styled from "styled-components"
-import { contexto } from "../../Context/Context"
+import { useContext, useEffect } from "react";
+import styled from "styled-components";
+import { contexto } from "../../Context/Context";
 
 export default function Hoje() {
-    const {setVisibilidade}=useContext(contexto)
-    const navigate = useNavigate()
+    const { setVisibilidade } = useContext(contexto);
 
-    useEffect(()=>{
-        setVisibilidade(true)
-        navigate('/habitos')
-    },[])
+    useEffect(() => {
+        setVisibilidade(true);
+    }, []);
 
     return (
         <HojeContainer>
-        
+
         </HojeContainer>
     )
-};
+}
 
-const HojeContainer=styled.div`
-    background: #E5E5E5;
-    height: 100%;
-`
+const HojeContainer = styled.div`
+    background: #e5e5e5;
+    min-height: 100vh;
+    padding: 20px 15px 120px;
+    div {
+        display: flex;
+        justify-content: space-between;
+    }
+    h1 {
+        font-size: 23px;
+        color: #126ba5;
+    }
+    button {
+        width: 40px;
+        height: 35px;
+        background: #52b6ff;
+        border-radius: 4.63636px;
+        border: none;
+        color: white;
+        font-size: 26.976px;
+    }
+    .container-cards {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 7px;
+    }
+    p {
+        font-size: 17.976px;
+        color: #666666;
+    }
+`;
